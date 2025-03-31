a0aab8a8cd224580af56021cb2368c398037cea6
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-03-31 16:38:07 +0530

    created employee api

--- companyHeadq/api/models.py
+++ companyHeadq/api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
-# company models
+# Company Model
 class Company(models.Model):
     company_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=60)
@@ -13,5 +12,21 @@ class Company(models.Model):
         ('Healthcare', 'Healthcare Company'),
         ('Education', 'Education Institute'),
         ('Other', 'Other')
-        ))
+    ))
     active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name + self.location
+
+# Employee Model
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)  
+    address = models.CharField(max_length=200)  
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    type = models.CharField(max_length=100, choices=(
+        ('Manager', 'Manager'),
+        ('Software Developer', 'SD'),
+        ('Project leader', 'PL'),
+    ))
+    company = models.ForeignKey(Company, on_delete=models.CASCADE) 
--- companyHeadq/api/serializers.py
+++ companyHeadq/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import  serializers
-from .models import Company
+from .models import Company, Employee
 
 #comapny serializers
 
@@ -8,5 +8,15 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+
 
 
